0b717529c70fca1c254118e850bad2eb8818e9b9
mani254 <manifreelancer25@gmail.com>
2024-07-26 20:29:10 +0530

    fixed the issues in the customer component

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Pagination from "../Pagination/Pagination";
-import { TextInput } from "../FormComponents/FormComponents";
+import { TextInput, SelectInput } from "../FormComponents/FormComponents";
 import Loader from "../Loader/Loader";
 import { connect } from "react-redux";
 import { showNotification } from "../../redux/notification/notificationActions";
+import { getStores } from "../../redux/stores/storeActions";
 
-function Customers({ showNotification }) {
+function Customers({ showNotification, storesData, getStores, auth }) {
   const [customers, setCustomers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalCustomersCount, setTotalCustomersCount] = useState(0);
@@ -15,21 +16,44 @@ function Customers({ showNotification }) {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const [debounceTimeout, setDebounceTimeout] = useState(null);
+  const [optionStores, setOptionStores] = useState([]);
+  const [filters, setFilters] = useState({
+    searchValue: "",
+    storeId: "",
+  });
+
+  useEffect(() => {
+    fetchStores();
+  }, []);
+
+  useEffect(() => {
+    setStoresOptions();
+  }, [storesData]);
+
+  useEffect(() => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+    const timeout = setTimeout(() => {
+      setFilters((prev) => ({ ...prev, searchValue: searchValue }));
+    }, 400);
+    setDebounceTimeout(timeout);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [searchValue]);
 
   useEffect(() => {
     fetchCustomers(currentPage);
   }, [currentPage]);
 
   useEffect(() => {
-    if (searchValue.length > 2) {
-      if (debounceTimeout) clearTimeout(debounceTimeout);
-      const timeout = setTimeout(() => {
-        fetchCustomers(1);
-        setCurrentPage(1);
-      }, 400);
-      setDebounceTimeout(timeout);
+    if (currentPage == 1) {
+      fetchCustomers(currentPage);
+    } else {
+      setCurrentPage(1);
     }
-  }, [searchValue]);
+  }, [filters]);
 
   const fetchCustomers = async (page = 1) => {
     setLoading(true);
@@ -43,7 +67,8 @@ function Customers({ showNotification }) {
           params: {
             limit,
             skip,
-            search: searchValue,
+            search: filters.searchValue,
+            storeId: filters.storeId,
           },
         },
       );
@@ -61,15 +86,49 @@ function Customers({ showNotification }) {
     }
   };
 
-  const detailedCustomerView = (orderId) => {
-    navigate(`/customers/view/${orderId}`);
+  function setStoresOptions() {
+    if (storesData.stores.length >= 1) {
+      const options = storesData.stores.map((item) => ({
+        value: item._id,
+        label: item.name,
+      }));
+      options.unshift({ value: "", label: "All" });
+      setOptionStores(options);
+    }
+  }
+
+  const fetchStores = async () => {
+    try {
+      await getStores();
+      console.log("stores-fetched succesfully");
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  //   const detailedCustomerView = (orderId) => {
+  //     navigate(`/customers/view/${orderId}`);
+  //   };
 
   return (
     <>
       <div className="mt-3 flex items-center justify-between border-b-2 pb-2">
         <h5>Customers</h5>
         <div className="flex">
+          {auth.user?.superAdmin && (
+            <SelectInput
+              options={optionStores}
+              label="Store"
+              id="Store"
+              name="storeId"
+              defaultValue={filters.storeId}
+              onChange={(e) =>
+                setFilters((prev) => ({ ...prev, storeId: e.target.value }))
+              }
+              variant="variant-1"
+              required
+            />
+          )}
           <TextInput
             onBlur={() => {
               fetchCustomers();
@@ -96,21 +155,15 @@ function Customers({ showNotification }) {
               <th>SL.NO</th>
               <th>Name</th>
               <th>Number</th>
-              <th>Total Orders</th>
             </tr>
           </thead>
           <tbody>
             {customers.length >= 1 &&
               customers.map((customer, index) => (
-                <tr
-                  key={customer._id}
-                  onClick={(e) => detailedCustomerView(customer._id)}
-                  className="transition hover:bg-gray-100"
-                >
+                <tr key={customer._id}>
                   <td>{index + 1}</td>
                   <td>{customer.name}</td>
                   <td>{customer.number}</td>
-                  <td>{customer.totalOrders}</td>
                 </tr>
               ))}
           </tbody>
@@ -133,11 +186,18 @@ function Customers({ showNotification }) {
     </>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    storesData: state.store,
+    auth: state.auth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     showNotification: (message) => dispatch(showNotification(message)),
+    getStores: () => dispatch(getStores()),
   };
 };
 
-export default connect(null, mapDispatchToProps)(Customers);
+export default connect(mapStateToProps, mapDispatchToProps)(Customers);
