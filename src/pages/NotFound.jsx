c5a28e3dca4a72f14683d51a8657cf2972469680
mani254 <manifreelancer25@gmail.com>
2024-07-26 23:55:05 +0530

    chanegs in notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <div>
-        <h1 className="text-center text-8xl">404</h1>
-        <h4 className="mt-2 text-center text-3xl">Page Not Found</h4>
-        <p className="mt-3 text-center text-xl">
+      <div className="text-center">
+        <h1 className="text-center text-3xl lg:text-8xl">404</h1>
+        <h4 className="mt-2 text-center text-xl lg:text-3xl">Page Not Found</h4>
+        <p className="mt-3 text-center text-lg lg:text-xl">
           The page That you are looking for not available please go to the home
           page
         </p>
-        <button className="mt-2 rounded-md px-3 py-2"></button>
+        <button
+          className="mt-2 rounded-md bg-violet-500 px-4 py-2 text-white"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Home
+        </button>
       </div>
     </div>
   );
